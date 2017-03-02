f42d50b8ffe3cddf2949ea57aca988d3e1cd1d0e
muirblake <muirblake@yahoo.com>
2017-03-02 17:17:19 -0600

    Update decrementer.v

--- decrementer.v
+++ decrementer.v
@@ -1,11 +1,23 @@
-module decrementer(CLK, valOut, decOut);
+module Lab4decrementer(CLK, valOut, decOut, displayFlag);
 input CLK;
 input [13:0] valOut;
 output reg [13:0] decOut;
+output reg displayFlag;
 
 always @(posedge CLK)
 begin
 	decOut = (valOut > 0) ? valOut-1: valOut;
+	if (decOut <= 200)begin
+		if ((decOut % 2) == 1)begin
+			displayFlag = 0;
+			end
+		else begin
+			displayFlag = 1;
+			end
+		end
+	else begin
+		displayFlag = 1;
+		end
 end
 
 endmodule
