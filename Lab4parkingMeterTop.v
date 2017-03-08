a1a9f358031045a71d9cbc34e94952672656f04e
muirblake <muirblake@yahoo.com>
2017-03-07 18:06:55 -0600

    Update Lab4parkingMeterTop.v

--- Lab4parkingMeterTop.v
+++ Lab4parkingMeterTop.v
@@ -4,30 +4,31 @@ output [3:0] an;
 output [6:0] seg;
 wire out1Hz, out1f2Hz, out62Hz, UPd, DWNd, LEFTd, RIGHTd, displayFlag;
 wire [3:0] Thous, Hund, Tens, Ones;
-wire [13:0] countOut, decOut;
+wire [13:0] contOut;
+//wire [3:0] an_temp;
 
 //Multiple clocks
 //Lab4clock c62hz(CLK, 1600000, out62Hz);
-//Lab4clock c62hz(CLK, 10000, out62Hz);
-//Lab4clock c1hz (CLK, 100000000, out1f2Hz);
-//Lab4clock c2hz (CLK, 50000000, out1Hz);
-
-Lab4clock c62hz(CLK, 2, out62Hz);
-Lab4clock c1hz (CLK, 20, out1f2Hz);
-Lab4clock c2hz (CLK, 10, out1Hz);
+Lab4clock c62hz(CLK, 10000, out62Hz);
+Lab4clock clocker(CLK, 50000000, slowCLK);
+//Lab4clock c1hz (CLK, 50000000, out1f2Hz);
+//Lab4clock c2hz (CLK, 25000000, out1Hz);
 
 //Decounce and synch inputs
-Lab4Debouncer updebounce(UP, UPd, CLK);
-Lab4Debouncer downdebounce(DWN, DWNd, CLK);
-Lab4Debouncer leftdebounce(LEFT, LEFTd, CLK);
-Lab4Debouncer rightdebounce(RIGHT, RIGHTd, CLK);
+//Lab4Debouncer updebounce(UP, UPd, CLK);
+//Lab4Debouncer downdebounce(DWN, DWNd, CLK);
+//Lab4Debouncer leftdebounce(LEFT, LEFTd, CLK);
+//Lab4Debouncer rightdebounce(RIGHT, RIGHTd, CLK);
 //Count and decrement
-Lab4counter count(decOut, SW0, SW1, UPd, LEFTd, RIGHTd, DWNd, countOut);
-Lab4decrementer decrement(out1f2Hz, countOut, decOut, displayFlag);
+//Lab4counter count(decOut, SW0, SW1, UP, LEFT, RIGHT, DWN, countOut);
+Lab4Controller cont(SW0, SW1, UP, LEFT, RIGHT, DWN, contOut, slowCLK, displayFlag);
+//Lab4decrementer decrement(out1f2Hz, countOut, decOut, displayFlag);
 //Convert to BCD
-Lab4BinaryToBCD convert(countOut, Thous, Hund, Tens, Ones);
+Lab4BinaryToBCD convert(contOut, Thous, Hund, Tens, Ones);
 //Convert to 7-Segment and Output
 Lab4BCDTo7Segment outToBoard(Thous, Hund, Tens, Ones, seg, an, out62Hz, displayFlag);
-
+// Flash on for one clock pulse and off for the next
+//Lab4Flasher flash(out1Hz, countOut, an_temp, an);
 endmodule
+
 
