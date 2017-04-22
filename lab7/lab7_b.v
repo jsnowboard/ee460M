6e1a6fa9d038b817b96355621c146e6b0c0f7bf3
Jeremiah Bartlett <jeremiah_bartlett@utexas.edu>
2017-04-22 16:54:34 -0500

    Finished JAL, MULT, most of MFHI and MFLO. Double check the last two.

--- lab7/lab7_b.v
+++ lab7/lab7_b.v
@@ -52,18 +52,21 @@ endmodule
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
+// Modified DR, SR1, and SR2 to make room for mhi and mLo
 
 module REG(CLK, RegW, DR, SR1, SR2, Reg_In, ReadReg1, ReadReg2);
   input CLK;
   input RegW;
-  input [4:0] DR;
-  input [4:0] SR1;
-  input [4:0] SR2;
+  input [5:0] DR;
+  input [5:0] SR1;
+  input [5:0] SR2;
   input [31:0] Reg_In;
   output reg [31:0] ReadReg1;
   output reg [31:0] ReadReg2;
 
-  reg [31:0] REG [0:31];
+  //parameter MHi = 6'd33, MLo = 6'd32;
+
+  reg [31:0] REG [0:33];
   integer i;
 
   initial begin
@@ -90,8 +93,6 @@ endmodule
 ///////////////////////////////////////////////////////////////////////////////
 
 `define opcode instr[31:26]
-`define sr1 instr[25:21]
-`define sr2 instr[20:16]
 `define f_code instr[5:0]
 `define numshift instr[10:6]
 
@@ -102,25 +103,37 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
   inout [31:0] Mem_Bus;
 
   //special instructions (opcode == 000000), values of F code (bits 5-0):
-  parameter add = 6'b100000;
-  parameter sub = 6'b100010;
+  parameter add  = 6'b100000;
+  parameter sub  = 6'b100010;
   parameter xor1 = 6'b100110;
   parameter and1 = 6'b100100;
-  parameter or1 = 6'b100101;
-  parameter slt = 6'b101010;
-  parameter srl = 6'b000010;
-  parameter sll = 6'b000000;
-  parameter jr = 6'b001000;
+  parameter or1  = 6'b100101;
+  parameter slt  = 6'b101010;
+  parameter srl  = 6'b000010;
+  parameter sll  = 6'b000000;
+  parameter jr   = 6'b001000;
+     // new instructions
+  parameter mult = 6'b011000;
+  parameter mfhi = 6'b010000;
+  parameter mflo = 6'b010010;
+  parameter add8 = 6'b101101;
+  parameter rbit = 6'b101111;
+  parameter rev  = 6'b110000;
+  parameter sadd = 6'b110001;
+  parameter ssub = 6'b110010;
 
   //non-special instructions, values of opcodes:
   parameter addi = 6'b001000;
   parameter andi = 6'b001100;
-  parameter ori = 6'b001101;
-  parameter lw = 6'b100011;
-  parameter sw = 6'b101011;
-  parameter beq = 6'b000100;
-  parameter bne = 6'b000101;
-  parameter j = 6'b000010;
+  parameter ori  = 6'b001101;
+  parameter lw   = 6'b100011;
+  parameter sw   = 6'b101011;
+  parameter beq  = 6'b000100;
+  parameter bne  = 6'b000101;
+  parameter j    = 6'b000010;
+    // new instructions
+  parameter jal  = 6'b000011;
+  parameter lui  = 6'b001111;
 
   //instruction format
   parameter R = 2'd0;
@@ -138,19 +151,37 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
   reg fetchDorI;
   wire [4:0] dr;
   reg [2:0] state, nstate;
+  // registers for the multiply
+  reg [31:0] mhi, mlo, mlo_save, mhi_save;
+  parameter MHi = 6'd33, MLo = 6'd32;
 
   //combinational
   assign imm_ext = (instr[15] == 1)? {16'hFFFF, instr[15:0]} : {16'h0000, instr[15:0]};//Sign extend immediate field
-  assign dr = (format == R)? instr[15:11] : instr[20:16]; //Destination Register MUX (MUX1)
+  // added J format for jal
+  assign dr = (format == R)? ( 
+                              (`f_code == mult)? (
+                                                   (state == 3'd3)? MHi : MLo
+                                                 ) : {1'b0,(instr[15:11])} 
+                              ) : (
+                                   (format == J)? 6'd31 : {1'b0,instr[20:16]}
+                                   ); //Destination Register MUX (MUX1)
   assign alu_in_A = readreg1;
   assign alu_in_B = (reg_or_imm_save)? imm_ext : readreg2; //ALU MUX (MUX2)
-  assign reg_in = (alu_or_mem_save)? Mem_Bus : alu_result_save; //Data MUX
-  assign format = (`opcode == 6'd0)? R : ((`opcode == 6'd2)? J : I);
+  assign reg_in = (alu_or_mem_save)? Mem_Bus : (  
+                                                 (`f_code == mult)? 
+                                                 (
+                                                   (state == 3'd3)? mhi_save : mlo_save
+                                                 ) : (alu_result_save)
+                                                ); //Data MUX
+  // added 6'd3 to deal with jal
+  assign format = (`opcode == 6'd0)? R : (((`opcode == 6'd2) || (`opcode == 6'd3))? J : I);
   assign Mem_Bus = (writing)? readreg2 : 32'bZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ;
+  assign sr1 = (`f_code == mfhi)? MHi : ((`f_code == mflo)? MLo : {1'b0, instr[25:21]});
+  assign sr2 = {1'b0, instr[20:16]};
 
   //drive memory bus only during writes
   assign ADDR = (fetchDorI)? pc : alu_result_save[6:0]; //ADDR Mux
-  REG Register(CLK, regw, dr, `sr1, `sr2, reg_in, readreg1, readreg2);
+  REG Register(CLK, regw, dr, sr1, sr2, reg_in, readreg1, readreg2);
 
   initial begin
     op = and1; opsave = and1;
@@ -166,7 +197,8 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
   always @(*)
   begin
     fetchDorI = 0; CS = 0; WE = 0; regw = 0; writing = 0; alu_result = 32'd0;
-    npc = pc; op = jr; reg_or_imm = 0; alu_or_mem = 0; nstate = 3'd0;
+    npc = pc; op = jr; reg_or_imm = 0; alu_or_mem = 0; nstate = 3'd0; mlo = 32'd0;
+    mhi = 32'd0;
     case (state)
       0: begin //fetch
         npc = pc + 7'd1; CS = 1; nstate = 3'd1;
@@ -175,11 +207,15 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
       1: begin //decode
         nstate = 3'd2; reg_or_imm = 0; alu_or_mem = 0;
         if (format == J) begin //jump, and finish
-          npc = instr[6:0];
-          nstate = 3'd0;
+          if (`opcode == j) begin
+            // npc = {pc[31:26], instr[25:0]};
+            npc = instr[6:0];
+            nstate = 3'd0;
+          end
         end
-        else if (format == R) //register instructions
+        else if (format == R) begin//register instructions
           op = `f_code;
+        end
         else if (format == I) begin //immediate instructions
           reg_or_imm = 1;
           if(`opcode == lw) begin
@@ -205,6 +241,13 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
         else if (opsave == sll) alu_result = alu_in_B << `numshift;
         else if (opsave == slt) alu_result = (alu_in_A < alu_in_B)? 32'd1 : 32'd0;
         else if (opsave == xor1) alu_result = alu_in_A ^ alu_in_B;
+        // added for jal
+        else if (opsave == jal) begin 
+          alu_result = pc;
+          npc = instr[6:0];
+        end
+        // added for mult
+        else if (opsave == mult) {mhi, mlo} = alu_in_A * alu_in_B; 
         if (((alu_in_A == alu_in_B)&&(`opcode == beq)) || ((alu_in_A != alu_in_B)&&(`opcode == bne))) begin
           npc = pc + imm_ext[6:0];
           nstate = 3'd0;
@@ -217,7 +260,12 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
       end
       3: begin //prepare to write to mem
         nstate = 3'd0;
-        if ((format == R)||(`opcode == addi)||(`opcode == andi)||(`opcode == ori)) regw = 1;
+        if ((format == R)||(`opcode == addi)||(`opcode == andi)||(`opcode == ori)||(`opcode == jal)) regw = 1;
+        // added for mult
+        else if (`opcode == mult) begin
+          regw = 1;
+          nstate = 3'd4;
+        end
         else if (`opcode == sw) begin
           CS = 1;
           WE = 1;
@@ -232,6 +280,11 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
         nstate = 3'd0;
         CS = 1;
         if (`opcode == lw) regw = 1;
+        // added for mult
+        else if (`opcode == mult) begin
+          CS = 0;
+          regw = 1;
+        end
       end
     endcase
   end //always
@@ -253,7 +306,11 @@ module MIPS (CLK, RST, CS, WE, ADDR, Mem_Bus);
       reg_or_imm_save <= reg_or_imm;
       alu_or_mem_save <= alu_or_mem;
     end
-    else if (state == 3'd2) alu_result_save <= alu_result;
+    else if (state == 3'd2) begin
+      mlo_save <= mlo;
+      mhi_save <= mhi;
+      alu_result_save <= alu_result;
+    end
 
   end //always
 
